a0710f0017024adbba2d2a9742f441af09e1f57d
AdamAbd <adamabd14113@gmail.com>
2023-11-06 13:01:52 +0700

    fix width

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container mx-auto px-5 my-10 flex flex-col gap-5">
+    <h1 class="text-4xl font-bold">Vue Todos</h1>
+    <p>
+      Vue Todos is a CRUD Todo tracking application created for a Vue 3 Crash Course by John
+      Komarnicki. <br />
+      Track your daily task's using this todo tracker with abilities to save, complete, edit &
+      delete todos. <br />
+      Feel free to add on to this application with functionalities such as the ability to drag and
+      reorder todos, animate creation/deletion of todos or even create this into a full stack
+      application with the ability to have different lists for different days of tracking todos!
+    </p>
   </div>
 </template>
 
--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch, computed } from 'vue'
 import { uid } from 'uid'
 import { Icon } from '@iconify/vue'
 
@@ -7,6 +7,18 @@ import TodoCreator from '../components/TodoCreator.vue'
 import TodoItem from '../components/TodoItem.vue'
 
 const todoList = ref([])
+
+watch(
+  todoList,
+  () => {
+    setTodoListLocalStorage()
+  },
+  { deep: true }
+)
+
+const todosCompleted = computed(() => {
+  return todoList.value.every((todo) => todo.isCompleted)
+})
 
 const fetchTodoList = () => {
   const saveTodoList = JSON.parse(localStorage.getItem('todoList'))
@@ -29,37 +41,27 @@ const createTodo = (todo) => {
     isCompleted: null,
     isEditing: null
   })
-
-  setTodoListLocalStorage()
 }
 
 const toggleTodoComplete = (todoPos) => {
   todoList.value[todoPos].isCompleted = !todoList.value[todoPos].isCompleted
-
-  setTodoListLocalStorage()
 }
 
 const toggleEditTodo = (todoPos) => {
   todoList.value[todoPos].isEditing = !todoList.value[todoPos].isEditing
-
-  setTodoListLocalStorage()
 }
 
 const updateTodo = (todoVal, todoPos) => {
   todoList.value[todoPos].todo = todoVal
-
-  setTodoListLocalStorage()
 }
 
 const deleteTodo = (todoId) => {
   todoList.value = todoList.value.filter((todo) => todo.id !== todoId)
-
-  setTodoListLocalStorage()
 }
 </script>
 
 <template>
-  <main class="flex flex-col max-w-lg w-full px-10 py-4">
+  <main class="flex flex-col max-w-lg mx-auto w-full px-10 py-4">
     <h1 class="mb-4 text-center text-xl font-medium">Create Todo</h1>
     <TodoCreator @create-todo="createTodo" />
     <ul v-if="todoList.length > 0" class="flex flex-col mt-6 gap-5">
@@ -77,6 +79,13 @@ const deleteTodo = (todoId) => {
     <p v-else class="flex items-center justify-center gap-2 mt-6">
       <Icon icon="noto-v1:sad-but-relieved-face" />
       <span>You have no todo's to complete! Add one!</span>
+    </p>
+    <p
+      v-if="todosCompleted && todoList.length > 0"
+      class="flex items-center justify-center gap-2 mt-6"
+    >
+      <Icon icon="noto-v1:party-popper" />
+      <span>You have completed all your todos!</span>
     </p>
   </main>
 </template>
